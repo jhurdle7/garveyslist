f16a5ac12ddcbb90f7a060376aed73a5e7f86c59
jhurdle7 <63259028+jhurdle7@users.noreply.github.com>
2020-07-01 13:51:34 -0700

    migrations

--- garveyslist/main/models.py
+++ garveyslist/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Business(models.Model):
+    business_type = models.ForeignKey
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    rating = models.IntegerField()
+    
+class BusinessType():
+    name = models.CharField(max_length=200)
+
+
+
+
+
--- garveyslist/main/views.py
+++ garveyslist/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    context = {'message': 'hello world!'
+
+    }
+    return render(request, 'main/index.html', context)
