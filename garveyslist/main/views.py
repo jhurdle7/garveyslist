18d7c2855de855bd47f9924d639e1f67bc213911
jhurdle7 <63259028+jhurdle7@users.noreply.github.com>
2020-07-02 16:21:47 -0700

    working through

--- garveyslist/main/views.py
+++ garveyslist/main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from djano.models import Business 
 from django.utils import timezone
 
 
@@ -10,11 +9,16 @@ from django.utils import timezone
 
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'main/index.html'
+    context_object_name = 'businesses'
+
+    def get_queryset(self):
+        return Business.objects.fiter(pub_date_lte=timezone.now()).order_by('-pub_date')
 
 def index(request):
-    business = Business.objects.
     context = {'message': 'hello world!'
-    'Business'
+
 
     }
     return render(request, 'main/index.html', context)
