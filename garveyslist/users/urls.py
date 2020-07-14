8e50dd8d3328a89d6c096a046f9807d055ba4ea0
jhurdle7 <63259028+jhurdle7@users.noreply.github.com>
2020-07-14 16:30:58 -0700

    user management

---
+++ garveyslist/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from . import views
+
+
+app_name= 'users'
+urlpatterns = [
+    path('login/', views.login_page, name='login_page'),
+    path('register/', views.register_page, name='register_page'),
+    path('logout/', views.logout_user, name='logout_user'),
+  
+  ]
--- garveyslist/users/views.py
+++ garveyslist/users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'users/home.html')
+
+def login_page(request):
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password =  request.POST['password']
+
+        user = authenticate(request, username=username, password = password)
+        if user is None: 
+            return reder(request, 'users/login.html', {'message': ' there is no User with that name or password'})
+            login(request, user)
+            if 'next' in request.GET:
+                return HttpResponseRedirect(request.GET['next'])
+            return HttpResponseRedirect(reverse('users:home'))
+
+        return render(request, 'users/login.html')
+
+def register_page(request):
+    if request.method == 'POST':
+
+        username - request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        retype_password = request.POST['retype_password']
+
+        if password != retype_password:
+            return render(request, 'users/register.html', {'message:': 'passwords not match'})
+        if User.objects.filter(username=username).exist():
+            return render(request, 'users/register.html', {'message':' a user with that username already exist'})
+        user - User.objects.create_user(username, email, passowrd)
+        login(request, user)
+        return render(request, user)
+
+        return HttpResponseRedirect(reverse('users:home'))
+        
+    return render(request, 'users/register.html')
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login_page'))
